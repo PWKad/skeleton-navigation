f037152603dcca414713b54f355f374de813bbc0
pwkad <patrickwaltersc21@yahoo.com>
2018-07-02 22:19:28 -0500

    Add focus

--- skeleton-esnext/src/welcome.js
+++ skeleton-esnext/src/welcome.js
@@ -9,7 +9,7 @@ export class Welcome {
     this.controller = controllerFactory.createForCurrentScope();
     this.controller.validateTrigger = validateTrigger.changeOrBlur;
   }
-  activate() {
+  attached() {
     this.hasFocus = true;
   }
 }
